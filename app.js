93506dc869b2bf607b867ab60d24984143b20bcd
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-12-03 13:59:08 -0300

    [feat] fetch characters

--- app.js
+++ app.js
@@ -16,7 +16,31 @@ async function fetchCharactersByPage(page = 1){
   const response = await axios.get(`${URL}/?page=${page}`)
   const characters = response.data.results;
 
+
+  // trtar erros
   console.log(characters);
+  characters.forEach(({name, status, location, image, episode, species}) => {
+    container.innerHTML += 
+      `
+      <article class="card">
+      <img class="character-image" src="${image}" alt="Character image">
+      <div class="character-info">
+          <div>
+              <h2>${name}</h2>
+              <h3><span class="status ${status}"></span>${status} - ${species}</h3>
+          </div>
+          <div>
+              <p>Última localização conhecida:</p>
+              <h3>${location.name}</h3>
+          </div>
+          <div>
+              <p>Visto a última vez em:</p>
+              <h3>${"sd"}</h3>
+          </div>
+      </div>
+    </article>
+      `
+  });
 }
 
 fetchCharactersByPage()
