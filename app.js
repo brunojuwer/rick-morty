797aa83ac5cac865e89fe761feb730f28d73f576
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-12-03 22:10:34 -0300

    [feat] create method for filter characters

--- app.js
+++ app.js
@@ -11,35 +11,35 @@ filter.addEventListener('blur', () => {
 }) 
 
 
+function mountCard(image, name, status, species, location, episode) {
+  return `
+  <article class="card">
+  <img class="character-image" src="${image}" alt="Character image">
+  <div class="character-info">
+      <div>
+          <h2>${name}</h2>
+          <h3><span class="status ${status}"></span>${status} - ${species}</h3>
+      </div>
+      <div>
+          <p>Última localização conhecida:</p>
+          <h3>${location.name}</h3>
+      </div>
+      <div>
+          <p>Visto a última vez em:</p>
+          <h3>${"episode"}</h3>
+      </div>
+  </div>
+</article>
+  `
+}
+
 // Fetch 
 async function fetchCharactersByPage(page = 1){
   const response = await axios.get(`${URL}/?page=${page}`)
   const characters = response.data.results;
-
-
   // trtar erros
-  console.log(characters);
   characters.forEach(({name, status, location, image, episode, species}) => {
-    container.innerHTML += 
-      `
-      <article class="card">
-      <img class="character-image" src="${image}" alt="Character image">
-      <div class="character-info">
-          <div>
-              <h2>${name}</h2>
-              <h3><span class="status ${status}"></span>${status} - ${species}</h3>
-          </div>
-          <div>
-              <p>Última localização conhecida:</p>
-              <h3>${location.name}</h3>
-          </div>
-          <div>
-              <p>Visto a última vez em:</p>
-              <h3>${"sd"}</h3>
-          </div>
-      </div>
-    </article>
-      `
+    container.innerHTML += mountCard(image, name, status, species, location, episode)
   });
 }
 
@@ -48,11 +48,18 @@ fetchCharactersByPage()
 
 // Filter characters by name
 
-// async function getCharactersByName(name) {
-//   return await axios.get(`${URL}/?name=${name}`)
-// }
+async function getCharactersByName(name) {
+  return await axios.get(`${URL}/?name=${name}`);
+}
 
-// filter.addEventListener('keyup', async e => {
-//   (await getCharactersByName(e.target.value)).data.results
-// }) 
+filter.addEventListener('keyup', e => {
+  container.innerHTML = ""
+  getCharactersByName(e.target.value)
+    .then(res => {
+        res.data.results.forEach(({name, status, location, image, episode, species}) => {
+          container.innerHTML += mountCard(image, name, status, species, location, episode)
+        });
+    });
+  
+}) 
 
