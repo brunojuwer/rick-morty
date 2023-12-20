b088046644d91914b78db51bbbb74c581aa5ee34
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-12-20 15:04:23 -0300

    [update] change styles of pagination

--- pagination/page-arrows.js
+++ pagination/page-arrows.js
@@ -1,6 +1,6 @@
-document.querySelector('.back-page-button').addEventListener('mouseover', () => {
+document.querySelector('.arrow-page-button').addEventListener('mouseover', () => {
   document.querySelector('.ph-arrow-fat-left').classList.add('move-left-arrow');
 });
-document.querySelector('.back-page-button').addEventListener('mouseout', () => {
+document.querySelector('.arrow-page-button').addEventListener('mouseout', () => {
   document.querySelector('.ph-arrow-fat-left').classList.remove('move-left-arrow');
 });
